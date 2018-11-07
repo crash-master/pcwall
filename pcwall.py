d058e8d927c7b1d9a128dd2fdb65dc69a698de35
s_eugene_r <s_eugene_r@yahoo.com>
2018-11-07 03:06:34 +0200

    beauty code

--- pcwall.py
+++ pcwall.py
@@ -1,48 +1,42 @@
-import requests
-import urllib
-import sys
-import os
+import requests, urllib, sys, os
 
-q = "";
-out_path = "";
-if len(sys.argv) > 1:
-    if sys.argv[1] == "-h" or sys.argv[1] == "-help":
-        print("HELP \n");
-        print("args:\n");
-        print("\t-h or -help: call for help\n");
-        print("\t-s or -search: give me photos by my search query\n");
-        print("\t-out: path to out dir\n");
-        print("EXAMPLE: \n");
-        print("\t python pcwall.py -search cats -out C:/wallpaper\n");
-        pass
 
-    if sys.argv[1] == "-s" or sys.argv[1] == "-search":
-        q = sys.argv[2];
-        pass;
+# FUNCS
+def argController():
+    global q, out_path;
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "-h" or sys.argv[1] == "-help" or sys.argv[1] == "help":
+            #  print HELP
+            print("HELP \n");
+            print("args:\n");
+            print("\t-h or -help: call for help\n");
+            print("\t-s or -search: give me photos by my search query\n");
+            print("\t-out: path to out dir\n");
+            print("EXAMPLE: \n");
+            print("\t python pcwall.py -search cats -out C:/wallpaper\n");
+            pass
 
-    if sys.argv[3] == "-out":
-        out_path = sys.argv[4];
-        pass;
-else:
-    exit();
+        if sys.argv[1] == "-s" or sys.argv[1] == "-search":
+            q = sys.argv[2];
+            pass;
 
-if q == "":
-    exit();
+        if sys.argv[3] == "-out":
+            out_path = sys.argv[4];
+            pass;
+    else:
+        exit();
 
-if out_path == "":
-    exit();
-
-if not os.path.exists(out_path):
-    os.makedirs(out_path);
+    if q == "":
+        print("Error, read help");
+        exit();
+    if out_path == "":
+        print("Error, read help");
+        exit();
     pass
 
-total_pages = 1;
-client_id = "?client_id=111812491413a8045327ce7d8f9bdd0511c4aedfa3571b8b5133f65c79789703";
-url_photo_list = "https://api.unsplash.com/search/photos/" + client_id;
-page_item = 1;
-photo_counter = 0;
-query_str = "&query=" + q;
-while page_item <= total_pages:
+
+def loadPage(page_item):
+    global total_pages, photo_counter;
     page_num = "&page=" + str(page_item);
     get_photo_list_link = url_photo_list + page_num + query_str;
     print("** Try to get the page with photo " + get_photo_list_link + "\n");
@@ -60,14 +54,31 @@ while page_item <= total_pages:
             photos.append(photo["urls"]["full"]);
 
     for item in photos:
-        img_name = item.split("?");
-        img_name = img_name[0].split("/");
-        img_name = img_name[len(img_name) - 1];
-        img_name = img_name + ".jpg";
+        img_name = item.split("?")[0].split("/")[-1] + ".jpg";
         print("** GET photo " + item + "\n");
         img = urllib.request.urlretrieve(item, out_path + '/' + img_name);
         if img :
             photo_counter += 1;
+
+    pass
+
+# APP
+
+q = "";
+out_path = "";
+argController();
+if not os.path.exists(out_path):
+    os.makedirs(out_path);
+    pass
+
+total_pages = 1;
+client_id = "?client_id=111812491413a8045327ce7d8f9bdd0511c4aedfa3571b8b5133f65c79789703";
+url_photo_list = "https://api.unsplash.com/search/photos/" + client_id;
+page_item = 1;
+photo_counter = 0;
+query_str = "&query=" + q;
+while page_item <= total_pages:
+    loadPage(page_item);
     page_item += 1;
     print("************************\n");
     print("Go to next page if exists\n");
