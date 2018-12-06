e9c4e257e54975cffe2d0397491b51c95b64d28a
Unknown <oleggordiichenko@gmail.com>
2018-12-06 23:52:47 +0200

    minor

--- pcwall.py
+++ pcwall.py
@@ -9,15 +9,12 @@ import time
 
 def createParser ():
 	parser = argparse.ArgumentParser(
-		description = '''This is very useful program that allows you to get some new walpapers.
-		e.x. windows: 
-		python pcwall.py -s nature -out H:walpaper/nature 
-		or 
-		python pcwall.py -s nature -out walpaper/nature
-		for linux''',
-		epilog = '''(c) Eugene and Oleg 2018. Developers are not responsible for anything happen in case of this script usage  -)''')
-	# parser.add_argument('-s', '--search', required=True)
-	parser.add_argument('-o', '--out')  # ('-o', '--out', default='img')
+		description='''This is very useful program that allows you to get some new walpapers from www.unsplash.com.''',
+		epilog='''(c) Eugene and Oleg 2018. This app was wrote during boring weekends.'''
+	)
+	# in case of problems with Windows install Ubuntu ;-) or just change '.' for windows style of folder path
+	# '.' - mean current folder in Linux based OS from which pcwall.py script was launched
+	parser.add_argument('-o', '--out', default=".")
 	parser.add_argument('-n', '--number', nargs='?', type=int, default=1)
 	parser.add_argument('-s', '--search', nargs='+', type=str, required=True)  # -s wall sex drugs "rock n roll"
 
@@ -28,7 +25,7 @@ def loadPage(page_item, maxPhotosAmount):
 	global total_pages, photo_counter
 	page_num = "&page=" + str(page_item)
 	get_photo_list_link = url_photo_list + page_num + query_str
-	print("** Try to get the page with photo " + get_photo_list_link + "\n")
+	# print("** Try to get the page with photo " + get_photo_list_link + "\n")
 	response = requests.get(get_photo_list_link)
 	if response.status_code != 200:
 		print("! Something wrong, I can`t access to server")
@@ -36,7 +33,7 @@ def loadPage(page_item, maxPhotosAmount):
 	if response.headers['X-Ratelimit-Remaining'] == 0:
 		print("Rate Limit Exceeded. Try one more time in an hour.")
 		return -1
-	print("** " + str(response.headers['X-Ratelimit-Remaining']) + " requests left\n");
+	print("** " + str(response.headers['X-Ratelimit-Remaining']) + " requests left");
 	res = response.json()
 	total_pages = res["total_pages"]
 	res = res['results']
@@ -50,7 +47,7 @@ def loadPage(page_item, maxPhotosAmount):
 
 	for item in photos:
 		img_name = item.split("?")[0].split("/")[-1] + ".jpg"
-		print("** GET photo [" + str(photo_counter + 1) + '] ' + item + "\n")
+		print("*** GET photo [" + str(photo_counter + 1) + '] ' + item)
 		img = urllib.request.urlretrieve(item, out_path + '/' + img_name)
 		if img:
 			photo_counter += 1
@@ -71,10 +68,6 @@ if __name__ == '__main__':
 	if out_path is not None:
 		if not os.path.exists(out_path):
 			os.makedirs(out_path)
-	else:
-		# in case of problems with Windows install Ubuntu ;-) or just change '.' for windows style of folder path
-		# '.' - mean current folder in Linux based OS from which pcwall.py script was launched
-		out_path = "."
 
 	total_pages = 1
 	client_id = "?client_id=111812491413a8045327ce7d8f9bdd0511c4aedfa3571b8b5133f65c79789703"
@@ -84,18 +77,22 @@ if __name__ == '__main__':
 	general_count = 0
 	total_total_pages = 0
 
+	print("Images source is www.unsplash.com\n")
+
 	if topics:
-		for topics in topics:
+		for topic in topics:
 			photo_counter = 0
-			query_str = "&query=" + topics
+			query_str = "&query=" + topic
+			print("Query: " + str(topic) + "\n")
 			while page_item <= total_pages:
 				if loadPage(page_item, maxPhotosAmount) == -1:  # photos amount
 					break
 				page_item += 1
 
+			print("\n")
+
 			general_count += photo_counter
 			total_total_pages += total_pages
-		print("Query: " + str(topics))
 
 	print("Photos downloaded: " + str(general_count) + " from " + str(total_total_pages * 10) + " images")
 
